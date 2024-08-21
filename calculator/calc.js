3dd667cb2196ea9bae5962a9b4496076e0e08db1
Dirgh-Shah <dirghshah1112@gmail.com>
2024-08-21 16:28:51 +0530

    Update calc.js

--- calculator/calc.js
+++ calculator/calc.js
@@ -1,37 +1,3 @@
-/*const buttons = document.querySelectorAll('input[type="button"]');
-const answerDisplay = document.getElementById('answers');
-
-let currentInput = '';
-let operator = '';
-
-const percentButton = document.querySelector('.operators:last-child');
-percentButton.value = 'C';
-
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    const value = button.value;
-
-    if (value === '=') {
-      try {
-        const result = eval(currentInput);
-        answerDisplay.textContent = result;
-        currentInput = result.toString();
-      } catch (error) {
-        answerDisplay.textContent = 'Error';
-        currentInput = '';
-      }
-    } else if (value === 'C') {
-      currentInput = '';
-      answerDisplay.textContent = '';
-    } else if (['+', '-', '*', '/'].includes(value)) {
-      operator = value;
-      currentInput += value;
-    } else {
-      currentInput += value;
-      answerDisplay.textContent = currentInput;
-    }
-  });
-});*/
 const buttons = document.querySelectorAll('input[type="button"]');
 const answerDisplay = document.getElementById('answers');
 
